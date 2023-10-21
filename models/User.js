f8a1cbc689dd8d47f1c0169cd4b5b69c5cdebafe
Jake Provard <provardjake@gmail.com>
2023-10-20 20:18:00 -0600

    added models

---
+++ models/User.js
@@ -0,0 +1,45 @@
+const {Schema, model} = require("mongoose");
+
+
+const userSchema = new Schema(
+    {
+        username: {
+            type: String,
+            unique: true,
+            required: true,
+            trim: true
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            match: [/^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/, "Email address not valid"]
+        },
+        thoughts: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'thought'
+            }
+        ],
+        friends: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: "user"
+            }
+        ]
+    },
+    {
+        toJSON: {
+            virtuals: true
+        },
+        id: false
+    }
+);
+
+userSchema.virtual("friendCount").get(function(){
+    return this.friends.length;
+});
+
+const User = model('user', userSchema);
+
+model.exports = User;
